27c7b9c7561ddc4ef0e64da0dfe3afad5135d2f4
Mitchell.Huitema <mitchellhuitema@hotmail.com>
2019-10-30 16:37:17 +0100

    init commit

--- api.py
+++ api.py
@@ -2,11 +2,11 @@ import requests
 import json
 import pprint
 
-# apikey = 'oh7gzUCb'
-# apiurl = 'https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?'
-# params = dict(key=apikey, format=json)
-# request = requests.get(apiurl, params=params)
+apikey = 'oh7gzUCb'
+apiurl = 'https://www.rijksmuseum.nl/api/nl/collection/?key='
+params = dict(key=apikey, format=json)
+request = requests.get(apiurl, params=params)
 
-request = requests.get('https://www.rijksmuseum.nl/api/nl/collection/?key=oh7gzUCb&format=json')
+# request = requests.get('https://www.rijksmuseum.nl/api/nl/collection/?key=oh7gzUCb&format=json')
 data = request.json()
 pprint.pprint(data)
