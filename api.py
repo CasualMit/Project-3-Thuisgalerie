bc14fc583b03c9b693f5d76bbfb9ca098cbf84e0
Mitchell <mitchellhuitema@hotmail.com>
2019-10-30 19:06:48 +0100

    function will give dict

--- api.py
+++ api.py
@@ -1,12 +1,35 @@
 import requests
-import json
 import pprint
 
-apikey = 'oh7gzUCb'
-apiurl = 'https://www.rijksmuseum.nl/api/nl/collection/?key='
-params = dict(key=apikey, format=json)
-request = requests.get(apiurl, params=params)
+def api_request(key):
+    apikey = 'oh7gzUCb'
+    apiurl = 'https://www.rijksmuseum.nl/api/nl/collection'
+    params = dict(key=apikey, format='json')
+    url = apiurl
+    request = requests.get(url, params=params)
+    res = request.json()
+    return res[key]
 
-# request = requests.get('https://www.rijksmuseum.nl/api/nl/collection/?key=oh7gzUCb&format=json')
-data = request.json()
-pprint.pprint(data)
+
+def api_filter(key):
+    test = api_request(key)
+    new_format = dict()
+    for item in test:
+        art_data = dict()
+        # pprint.pprint(item)
+        # pprint.pprint(item['id'])
+        # pprint.pprint(item['title'])
+        # pprint.pprint(item['webImage']['url'])
+        # pprint.pprint(item['principalOrFirstMaker'])
+
+        art_data['titel'] = item['title']
+        art_data['artiest'] = item['principalOrFirstMaker']
+        art_data['image'] = item['webImage']['url']
+        new_format[item['id']] = art_data
+    pprint.pprint(new_format)
+
+    return
+
+api_filter('artObjects')
+
+# test = '/SK-C-5'
