405c2f89ee5455daa91542537b3eec5be4095199
Sherwin.Kooij <sherwinkooij@gmail.com>
2019-10-30 10:10:30 +0100

    init

--- sherwin.py
+++ sherwin.py
@@ -1,1 +1,8 @@
-print("hellogit")
+import tkinter
+
+window = tkinter.Tk()
+# to rename the title of the window
+window.title("GUI")
+# pack is used to show the object in the window
+label = tkinter.Label(window, text = "Hello World!").pack()
+window.mainloop()
