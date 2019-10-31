05f77db1b5d579fede436143198fd477b6c1a57f
Mitchell.Huitema <mitchellhuitema@hotmail.com>
2019-10-31 18:53:31 +0100

    init commit

--- api.py
+++ api.py
@@ -2,23 +2,23 @@ import requests
 import pprint
 import json
 
-def api_request(key):
+def api_request(key, cached=True):
     apikey = 'oh7gzUCb'
     apiurl = 'https://www.rijksmuseum.nl/api/nl/collection'
     params = dict(key=apikey, ps=2, format='json')
     request = requests.get(apiurl, params=params)
     res = request.json()
     pieces = res[key]
-    with open('test.txt', 'w') as outfile:
+    with open('cache.txt', 'w') as outfile:
         json.dump(pieces, outfile)
     return
 
 
 def api_filter():
-    with open('test.txt') as json_file:
-        test = json.load(json_file)
+    with open('cache.txt') as json_file:
+        cache = json.load(json_file)
     new_format = dict()
-    for item in test:
+    for item in cache:
         art_data = dict()
         art_data['titel'] = item['title']
         art_data['artiest'] = item['principalOrFirstMaker']
@@ -28,6 +28,10 @@ def api_filter():
         json.dump(new_format, outfile)
     return
 
+def new_request():
+    with open('cache.txt') as json_file:
+        cache = json.load(json_file)
+# check create date cache.txt
 
-print(api_request('artObjects'))
+print(api_request('artObjects', cached=False))
 print(api_filter())
