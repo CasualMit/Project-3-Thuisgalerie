ee87bd188ad32f8eb35d485ce56d38cd1d3dc9be
Mitchell.Huitema <mitchellhuitema@hotmail.com>
2019-11-01 16:02:53 +0100

    init commit

--- api.py
+++ api.py
@@ -5,10 +5,11 @@ import os
 import time
 # api_request('artObjects')
 
+
 def api_request(key):
     apikey = 'oh7gzUCb'
     apiurl = 'https://www.rijksmuseum.nl/api/nl/collection'
-    params = dict(key=apikey, ps=2, format='json')
+    params = dict(key=apikey, ps=20, format='json')
     request = requests.get(apiurl, params=params)
     res = request.json()
     pieces = res[key]
@@ -29,6 +30,37 @@ def check_cache(filename, key='artObjects'):
             return True
     else:
         api_request(key)
+
+def check_reservedfile():
+    while True:
+        if os.path.isfile('reserved.txt'):
+            with open('reserved.txt') as json_file:
+                reserved = json.load(json_file)
+            return reserved
+        else:
+            empty_json = dict()
+            with open('reserved.txt', 'w') as outfile:
+                json.dump(empty_json, outfile)
+
+
+def keep_reserved():
+    with open('cache.txt') as json_file:
+        cache = json.load(json_file)
+
+    reserved = check_reservedfile()
+
+    reserved_art_pieces = set()
+    for reserved_item in reserved:
+        if reserved_item in cache:
+            reserved_art_pieces.add(reserved_item)
+
+    available_art_pieces = dict()
+    for item in cache:
+        if item not in reserved_art_pieces and item != 'formatted':
+            available_art_pieces[item] = cache[item]
+    with open('available.txt', 'w') as outfile:
+        json.dump(available_art_pieces, outfile)
+
 
 
 def request(objects):
@@ -59,5 +91,9 @@ def request(objects):
         break
 
 
+
+
+
 request('artObjects')
+keep_reserved()
 
